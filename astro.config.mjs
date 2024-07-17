4a4110ce6ee6535e532ecc80aeff27bd01a449e1
Alvaro Fragoso <alvarofragosoc@hotmail.com>
2024-07-17 14:42:20 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://alexandranutri.com',
+  output: 'static',
   experimental: {
     viewTransitions: true,
   },
